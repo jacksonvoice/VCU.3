a051734b521d492b6bd6e518363a38d7f754563f
Daniel Jackson <jacksonvoice@yahoo.com>
2014-11-28 20:44:39 -0800

    added seed data for admin user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create(email: "jacksonvoice@yahoo.com", password: "password", password_confirmation: "password", role: 99)
