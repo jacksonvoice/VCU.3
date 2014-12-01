bb1a85437407ae5a62c32d065946f75a42ef9b2f
Daniel Jackson <jacksonvoice@yahoo.com>
2014-12-01 12:44:17 -0800

    added model associations

--- db/migrate/20141201203639_create_answers.rb
+++ db/migrate/20141201203639_create_answers.rb
@@ -1,8 +1,8 @@
 class CreateAnswers < ActiveRecord::Migration
   def change
     create_table :answers do |t|
-      t.id :question
-      t.string :content
+      t.integer :question_id
+      t.text :content
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,33 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141201191414) do
+ActiveRecord::Schema.define(version: 20141201203639) do
+
+  create_table "answers", force: true do |t|
+    t.integer  "question_id"
+    t.text     "content"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "courses", force: true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "name"
     t.string   "description"
+  end
+
+  create_table "questions", force: true do |t|
+    t.integer  "survey_id"
+    t.text     "content"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "quizzes", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "sections", force: true do |t|
