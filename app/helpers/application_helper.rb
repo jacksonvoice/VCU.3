802f4dcd2c1fd1b09bc4a552267239d2194be6ed
Daniel Jackson <jacksonvoice@yahoo.com>
2014-12-03 14:21:49 -0800

    got dynamic add fields and remove fields working for the quizzes#new action/view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,19 +5,17 @@ module ApplicationHelper
 			id = new_object.object_id
 			fields = f.fields_for(association, new_object, child_index: id) do |builder|
 				render(association.to_s.singularize + "_fields", f: builder)
-		end
-	
+			end
 		link_to(name, "#", class: "add_fields button small radius", data: {id: id, fields: fields.gsub("\n", "")})
 		end
+
 
 		def link_to_add_answer_field(name, f, association)
 			new_object = f.object.send(association).klass.new
 			id = new_object.object_id
 			fields = f.fields_for(association, new_object, child_index: id) do |builder|
 				render(association.to_s.singularize + "_fields", f: builder)
-		end
-
-
+			end
 			link_to(name, '#', class: "add_fields button small radius", data: {id: id, fields: fields.gsub("\n", "")})
 		end
 
