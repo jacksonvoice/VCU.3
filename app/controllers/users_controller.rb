9bb1312d4f584eb11b3e4b4ef78e49260ec5242d
Daniel Jackson <jacksonvoice@yahoo.com>
2014-12-23 14:32:23 -0800

    added fileupload and widget jquery libraries

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 class UsersController < ApplicationController
 load_and_authorize_resource
 
+	def new
+		@s3_direct_post = S3_BUCKET.presigned_post(key: "uploads/#{SecureRandom.uuid}/${filename}", success_action_status: 201, acl: :public_read)
+	end
+
 	def index
 		@user = User.all
 	end
